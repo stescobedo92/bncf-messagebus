1fb8effffaeaf13ca89b964092f6505f8b35aa42
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-03-27 16:51:24 -0600

    Added the New function

--- messagebus/messagebus.go
+++ messagebus/messagebus.go
@@ -8,3 +8,7 @@ type MessageBus struct {
 	lock     sync.Mutex
 	callback map[string][]chan interface{}
 }
+
+func New() *MessageBus {
+	return &MessageBus{callbacks: make(map[string][]chan interface{})}
+}
